709d25fe74bc3c901e867ee761ca99e779ad3af1
Rodrigo <rnascimentodecarva@unomaha.edu>
2014-09-10 17:50:56 -0500

    Mudando tudo novamente, hurray

--- MultiCastOrdenado/src/multicastordenado/MultiCastOrdenado.java
+++ MultiCastOrdenado/src/multicastordenado/MultiCastOrdenado.java
@@ -1,43 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Rodrigo Nascimento de Carvalho 380067
+ * Philippe Cesar Ramos 380415
+ * Classe que cria processos e passa a eles: o seu pid, o seu clock inicial, 
+ * a porta em que sua parte servidor irá rodar e um array contendo as portas da parte servidor dos outros processos
  */
 
 package multicastordenado;
-
 
 /**
  *
  * @author Rodrigo
  */
 public class MultiCastOrdenado {
-
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args){
-        
+        //Definindo um array de portas das quais os processos mandarão mensagem, nesse caso todos recebem mensagem, inclusive o proprio processo
+        int[] portArray = {50000, 50001, 50002}; 
         try {
+            //Classe Processo implementa a interface Runnable que é utilizada para criar uma thread.
+            Runnable p1 = new Process(1,2, 50000, portArray);
+            new Thread(p1).start();
             
-            Runnable r = new Server(1,2);
-            new Thread(r).start();
-            
-            
-            
-            Runnable q = new Server(3,5);
-            new Thread(q).start();
-            
-            Runnable r2 = new Client(1,2);
-            new Thread(r2).start();
-            
-            Runnable p2 = new Client(2,3);
+            Runnable p2 = new Process(2,3, 50001, portArray);
             new Thread(p2).start();
+            Runnable p3 = new Process(3,5, 50002, portArray);
+            new Thread(p3).start();
             
-            Runnable q2 = new Client(3,5);
-            new Thread(q2).start();
         }catch(Exception ex){
-            
+            System.out.print(ex.getMessage());
         }        
     }
     
